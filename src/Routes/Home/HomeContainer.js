d82f89822b2acb04e22dda12e8f25574566ac24a
MoonSangJin <1128msj@naver.com>
2020-12-28 02:37:18 +0900

    chore : storage API 이용 (google login+server function)

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -2,6 +2,13 @@ import React from 'react';
 import HomePresenter from './HomePresenter';
 
 const HomeScreenContainer = () => {
+  chrome.storage.sync.set({ key: 'im sangjin' }, function () {
+    console.log('Value is set to ');
+  });
+
+  chrome.storage.sync.get(['key'], function (result) {
+    console.log(result.key);
+  });
   return <HomePresenter></HomePresenter>;
 };
 export default HomeScreenContainer;
