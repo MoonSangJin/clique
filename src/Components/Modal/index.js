e419f9faf9e72fbfc9581c7978ef33dc84163d45
MoonSangJin <1128msj@naver.com>
2020-12-30 20:15:11 +0900

    fix : export 제거

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-export const Overlay = styled.div`
+const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
-  
+
   width: 100vw;
   height: 100vh;
   border: 1px solid black;
-  
+
   background-color: rgba(0, 0, 0, 0.16);
 `;
 
-export const ModalWrapper = styled.div`
+const ModalWrapper = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
@@ -27,19 +26,13 @@ export const ModalWrapper = styled.div`
   padding: 20px;
   border: solid 1px black;
   background-color: white;
-  
 `;
 
 export default function Modal({ isOpen, closeHandler, children }) {
-  return (
-    isOpen ?
-      <>
-        <Overlay isOpen={isOpen} onClick={closeHandler} />
-        <ModalWrapper>
-          {children}
-        </ModalWrapper>
-      </>
-      :
-      null
-  );
+  return isOpen ? (
+    <>
+      <Overlay isOpen={isOpen} onClick={closeHandler} />
+      <ModalWrapper>{children}</ModalWrapper>
+    </>
+  ) : null;
 }
