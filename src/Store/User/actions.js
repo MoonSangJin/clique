45edd2f1258cc086d6fac7597df50085f09d509f
maitracle <maitracle@gmail.com>
2021-01-06 23:17:49 +0900

    refactor: 예제용 store 코드를 제거한다.

--- src/Store/User/actions.js
+++ src/Store/User/actions.js
@@ -1,28 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
-
-export const DUMB_STATE_INCREASE = 'users/DUMB_STATE_INCREASE';
-
-export const DUMB_STATE_DECREASE = 'users/DUMB_STATE_DECREASE';
-
-export const DUMB_STATE_INCREASE_BY_AMOUNT = 'users/DUMB_STATE_INCREASE_BY_AMOUNT';
-
-
-export const DUMB_API_REQUEST = 'users/DUMB_API_REQUEST';
-export const DUMB_API_SUCCESS = 'users/DUMB_API_SUCCESS';
-export const DUMB_API_FAILURE = 'users/DUMB_API_FAILURE';
-
 export const SET_USER_INFO = 'users/SET_USER_INFO';
 
-
-export const dumbStateIncrease = createAction(DUMB_STATE_INCREASE);
-
-export const dumbStateDecrease = createAction(DUMB_STATE_DECREASE);
-
-export const dumbStateIncreaseByAmount = createAction(DUMB_STATE_INCREASE_BY_AMOUNT);
-
-export const dumbApiRequest = createAction(DUMB_API_REQUEST);
-export const dumbApiSuccess = createAction(DUMB_API_SUCCESS);
-export const dumbApiFailure = createAction(DUMB_API_FAILURE);
 
 export const setUserInfo = createAction(SET_USER_INFO);
--- src/Store/User/reducer.js
+++ src/Store/User/reducer.js
@@ -1,12 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
-import {
-  dumbApiFailure,
-  dumbApiSuccess,
-  dumbStateDecrease,
-  dumbStateIncrease,
-  dumbStateIncreaseByAmount,
-  setUserInfo,
-} from './actions';
+import { setUserInfo } from './actions';
 
 
 const initState = {
@@ -23,26 +16,6 @@ const initState = {
 
 
 const userReducer = createReducer(initState, {
-  [dumbStateIncrease]: (state) => ({
-    ...state,
-    dumbState: state.dumbState + 1,
-  }),
-  [dumbStateDecrease]: (state) => ({
-    ...state,
-    dumbState: state.dumbState - 1,
-  }),
-  [dumbStateIncreaseByAmount]: (state, action) => ({
-    ...state,
-    dumbState: state.dumbState + action.payload,
-  }),
-  [dumbApiSuccess]: (state, action) => ({
-    ...state,
-    imageUrl: action.payload,
-  }),
-  [dumbApiFailure]: (state) => ({
-    ...state,
-    imageUrl: '',
-  }),
   [setUserInfo]: (state, action) => ({
     ...state,
     user: {
