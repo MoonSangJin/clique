c36e9a06ab493494912639edd4b1b1294c31dfba
maitracle <maitracle@gmail.com>
2021-01-09 17:50:40 +0900

    style: main 페이지의 마진을 조정한다.

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -4,12 +4,13 @@ import Background from '../assets/img/background.svg';
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 200px;
+  height: 260px;
   background-image: url(${Background});
   background-size: cover;
   border-radius: 10px;
-  margin: 0 auto;
+  margin: 65px auto 0 auto;
 `;
+
 export default function background() {
   return <Wrapper />;
 }
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Gnb from '../../Components/Gnb';
 import Background from '../../Components/Background';
-import SearchInput from '../../Components/SearchInput';
 import FolderRow from '../../Components/FolderRow';
 import { Link } from 'react-router-dom';
+
 
 const HomeScreenPresenter = ({ data }) => {
   return (
@@ -15,22 +15,33 @@ const HomeScreenPresenter = ({ data }) => {
       <Link to="/sign-up/">sign-up</Link>
       <Wrapper>
         <Background />
-        <SearchRow>
-          <SearchInput />
-        </SearchRow>
-        <FolderRow {...{ data }} />
+        {/*Todo(maitracle): 데모데이 버전에서는 search 기능을 지원하지 않는다.*/}
+        {/*<SearchRow>*/}
+        {/*  <SearchInput />*/}
+        {/*</SearchRow>*/}
+        <FolderRowWrapper>
+          <FolderRow {...{ data }} />
+        </FolderRowWrapper>
+
       </Wrapper>
     </Container>
   );
 };
 const Container = styled.div``;
 
-const SearchRow = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+// Todo(maitracle): search 기능을 사용할 때 주석을 해재한다.
+// const SearchRow = styled.div`
+//   display: flex;
+//   justify-content: center;
+// `;
+
 const Wrapper = styled.div`
   width: 90%;
   margin: 0 auto;
 `;
+
+const FolderRowWrapper = styled.div`
+  margin-top: 47px;
+`;
+
 export default HomeScreenPresenter;
