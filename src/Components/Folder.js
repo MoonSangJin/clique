4859af571dfdf235ce9452d201db089479ccb41c
maitracle <maitracle@gmail.com>
2021-01-10 21:05:42 +0900

    style: folder style을 수정한다.

--- src/Components/Folder.js
+++ src/Components/Folder.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Example from '../assets/img/example.jpg';
 import folder from '../assets/img/folder.svg';
 import verticalLine from '../assets/img/verticalLine.svg';
-import isFavorite from '../assets/img/isFavorite.svg';
+import FavoriteIconSrc from '../assets/img/isFavorite.svg';
 import PopoverController from './Popover/PopoverController';
 import DropdownMenu from '../Modules/Folder/DropdownMenu';
 import OptionIcon from './OptionIcon';
@@ -32,73 +32,79 @@ export default function Folder({ folder_data }) {
   return (
     <Container>
       <FolderImage /> {/* 폴더 이미지 들어갈 곳*/}
-      <FavoriteRow>
-        {
-          favorite ? <IsFavorite src={isFavorite} /> : null
-        }
-      </FavoriteRow>
-      <TextRow>
-        <FolderName>{folder_title}</FolderName>
-        <FolderTime>{time}</FolderTime>
-      </TextRow>
-      <FaviconRow>
-        <Icon>
-          <DefaultFolder src={folder} alt={folder} />
-          <VerticalLine src={verticalLine} />
-          <FavIcon src={folder} />
-          <FavIcon src={folder} />
-          <FavIcon src={folder} />
-          <FavIcon src={folder} />
-          <FavIcon src={folder} />
-          <FavIcon src={folder} />
-        </Icon>
-        <PopoverController ref={dotMenuRef} onClick={openDropdownMenu}>
-          <OptionIcon />
-        </PopoverController>
 
-        <DropdownMenu
-          isOpen={isOpenDropdownMenu}
-          closeHandler={closeDropdownMenu}
-          anchorEl={profileElementHolder}
-          sharedText={mockedTextForShare}
-        />
-      </FaviconRow>
+      <ContentsWrapper>
+        <TitleWrapper>
+          <TextRow>
+            <FolderName>{folder_title}</FolderName>
+            <FolderTime>{time}</FolderTime>
+          </TextRow>
+          {
+            favorite ? <FavoriteIcon src={FavoriteIconSrc} /> : null
+          }
+        </TitleWrapper>
+        <MenuWrapper>
+          <FaviconWrapper>
+            <FaviconFolder src={folder} alt={folder} />
+            <VerticalLine src={verticalLine} />
+            <FavIcon src={folder} />
+            <FavIcon src={folder} />
+            <FavIcon src={folder} />
+            <FavIcon src={folder} />
+            <FavIcon src={folder} />
+            <FavIcon src={folder} />
+          </FaviconWrapper>
+          <PopoverController ref={dotMenuRef} onClick={openDropdownMenu}>
+            <OptionIcon />
+          </PopoverController>
+          <DropdownMenu
+            isOpen={isOpenDropdownMenu}
+            closeHandler={closeDropdownMenu}
+            anchorEl={profileElementHolder}
+            sharedText={mockedTextForShare}
+          />
+        </MenuWrapper>
+      </ContentsWrapper>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 372px;
-  height: 482px;
   display: flex;
   flex-direction: column;
+  width: 282px;
+  height: 366px;
+  margin-bottom: 20px;
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.08);
-  border-radius: 10px;
-
-  margin-bottom: 30px;
+  border-radius: 8px;
 `;
 
 const FolderImage = styled.div`
   width: 100%;
-  height: 279.42px;
+  height: 210px;
   background-image: url(${({ favIconUrl }) => favIconUrl || Example});
   background-size: cover;
-  border-radius: 10px 10px 0 0;
+  border-radius: 8px 8px 0 0;
 `;
 
-const FavoriteRow = styled.div`
-  text-align: right;
-  height: 46px;
+const ContentsWrapper = styled.div`
+  padding-left: 24px;
+  padding-right: 24px;
 `;
 
-const IsFavorite = styled.img`
-  width: 30px;
-  height: 46px;
-  margin-right: 30px;
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const TextRow = styled.div`
-  margin-left: 32px;
+  margin-top: 20px;
+  flex-grow: 1;
+`;
+
+const FavoriteIcon = styled.img`
+  width: 24px;
+  height: 37px;
 `;
 
 const FolderName = styled.div`
@@ -106,9 +112,8 @@ const FolderName = styled.div`
   font-family: Poppins;
   font-style: normal;
   font-weight: normal;
-  font-size: 20px;
-  line-height: 30px;
-
+  font-size: 14px;
+  line-height: 21px;
   letter-spacing: -0.02em;
 
   color: #070701;
@@ -118,37 +123,36 @@ const FolderName = styled.div`
 `;
 
 const FolderTime = styled.div`
-  display: block;
+  margin-top: 2px;
+
   font-family: Poppins;
   font-style: normal;
   font-weight: normal;
-  font-size: 16px;
-  line-height: 24px;
-
+  font-size: 12px;
+  line-height: 18px;
   letter-spacing: -0.02em;
-
-  color: #90a0ad;
+  color: #90A0AD;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
 
-const FaviconRow = styled.div`
+const MenuWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 60px 32px 0 32px;
+  margin-top: 45px;
 `;
 
-const Icon = styled.div`
+const FaviconWrapper = styled.div`
   display: flex;
   align-items: center;
-  max-width: 293px;
+  flex-grow: 1;
   overflow-x: hidden;
 `;
 
-const DefaultFolder = styled.img`
-  width: 30px;
-  height: 25px;
+const FaviconFolder = styled.img`
+  width: 23px;
+  height: 21px;
 `;
 
 const VerticalLine = styled.img`
@@ -157,6 +161,6 @@ const VerticalLine = styled.img`
 `;
 
 const FavIcon = styled.img`
-  width: 26px;
-  height: 26px;
+  width: 21px;
+  height: 21px;
 `;
