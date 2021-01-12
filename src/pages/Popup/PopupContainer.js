98e6f17dc0bc6863e0eff9808a8a093d35ecf928
MoonSangJin <1128msj@naver.com>
2021-01-12 21:08:18 +0900

    chore:mock_data를 제거한다

--- src/pages/Popup/PopupContainer.js
+++ src/pages/Popup/PopupContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PopupPresenter from './PopupPresenter';
 import axios from 'axios';
-import qs from 'qs';
+
 const token =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEwNTAxMTY0LCJqdGkiOiIxZWNlMWRkYjY5NDU0ZjYwOTM4ZWMwYzVjOTJiODZlZiIsInVzZXJfaWQiOjR9.PIu-1s6rGKTW1ClLiWYCVynQY0ccJV4Ml8yox_vgItY';
 
@@ -9,23 +9,6 @@ const PopupContainer = () => {
   const [tabs, setTabs] = useState([]);
   const searchUrl = () => {
     chrome.tabs.query({ lastFocusedWindow: true }, (tabs) => setTabs(tabs));
-  };
-
-  const mock_data = {
-    bookmarks: [
-      {
-        title: 'hi',
-        url: 'http://someurl598494811.com',
-        scroll_pos: 10,
-        favicon_url: 'http://www.naver.com',
-      },
-      {
-        title: 'hi2',
-        url: 'http://someurl55156511.com',
-        scroll_pos: 20,
-        favicon_url: 'http://www.naver.com',
-      },
-    ],
   };
 
   const postServer = async (state) => {
@@ -45,8 +28,8 @@ const PopupContainer = () => {
 
   useEffect(() => {
     searchUrl();
-    postServer(mock_data);
   }, []);
-  return <PopupPresenter {...{ tabs }}></PopupPresenter>;
+
+  return <PopupPresenter {...{ tabs, postServer }}></PopupPresenter>;
 };
 export default PopupContainer;
