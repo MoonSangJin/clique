2b6ac337291890727a82b231fc93d20ce9650896
maitracle <maitracle@gmail.com>
2021-01-19 03:14:25 +0900

    style: 데모데이를 위한 스타일 수정

--- src/Components/FolderRow.js
+++ src/Components/FolderRow.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import Folder from '../Components/Folder';
 import ListFolder from '../Components/ListFolder';
 import FolderListHeader from '../Modules/Folder/FolderListHeader';
+import FirstImageSrc from  '../assets/img/FolderItemImages/1.png';
+import SecondImageSrc from  '../assets/img/FolderItemImages/2.png';
+import ThirdImageSrc from  '../assets/img/FolderItemImages/3.png';
+import FourthImageSrc from  '../assets/img/FolderItemImages/4.png';
 
+
+const mapIndexToImageSrc = {
+  0: FirstImageSrc,
+  1: SecondImageSrc,
+  2: ThirdImageSrc,
+  3: FourthImageSrc,
+};
 
 export default function FolderRow({ bookmarkFolderList, type }) {
   return (
@@ -13,7 +25,7 @@ export default function FolderRow({ bookmarkFolderList, type }) {
         {
           type === 'card' ?
             bookmarkFolderList.map((folder_data, index) => {
-              return <Folder key={index} {...{ folder_data }} />;
+              return <Folder key={index} {...{ folder_data }} folderCoverImageSrc={mapIndexToImageSrc[index % 4]} />;
             }) :
             bookmarkFolderList.map((folder_data, index) => {
               return <ListFolder key={index} {...{ folder_data }} />;
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,11 +4,6 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 printLine("Using the 'printLine' function from the Print Module");
 
-let confirm_variable = confirm('저장된 스크롤 위치로 이동하시겠습니까?');
-confirm_variable
-  ? window.scrollTo({ top: 800, behavior: 'smooth' })
-  // Todo(maitracle): console.log 대신 printLine을 사용하면 confirm창이 뜨지 않는 버그가 없어지는 느낌... 해당 버그를 확인하고 해결한다.
-  : printLine('이동안함');
 
 let last_known_scroll_position = 0;
 let contentTitle = document.title;
