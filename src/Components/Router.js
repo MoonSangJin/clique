29a67ada70e91c9b3145a911c8a8e09763271dc9
MoonSangJin <1128msj@naver.com>
2021-01-29 01:11:27 +0900

    feat : page 이동 animation

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Routes/Home';
 import Detail from '../Routes/Detail';
 import SignIn from '../Routes/SignIn';
 import SignUp from '../Routes/SignUp';
 import Gnb from './Gnb';
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './Router.css';
+
 const RootRouter = () => (
   <Router>
     <Gnb />
-
-    <Route path="/" exact component={Home} />
-    <Route path="/detail/:folderId" component={Detail} />
-    <Route path="/sign-in/" component={SignIn} />
-    <Route path="/sign-up/" component={SignUp} />
+    <Route
+      render={({ location }) => {
+        console.log(location);
+        return (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames="fade">
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route path="/detail/:folderId" component={Detail} />
+                <Route path="/sign-in/" component={SignIn} />
+                <Route path="/sign-up/" component={SignUp} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        );
+      }}
+    ></Route>
   </Router>
 );
 
