d6a7029902b922243c2b54fae843035463510c63
maitracle <maitracle@gmail.com>
2021-02-07 17:40:32 +0900

    feat: not signed in icon menu 추가

--- src/pages/Popup/NotSignInPage.js
+++ src/pages/Popup/NotSignInPage.js
@@ -1,6 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import logoSrc from '../../assets/img/logo';
+
 
 const NotSignInPage = () => {
-  return <div>please sign in at Clique</div>;
+  return (
+    <Wrapper>
+      <Logo src={logoSrc} />
+      <Message>
+        Sign In at Clique to make bookmarks!
+      </Message>
+
+      <LinkToSignIn href={'/newtab.html#/sign-in/'} target={'_blank'} rel={'noreferrer'}>
+        Go to sign in
+      </LinkToSignIn>
+
+    </Wrapper>
+  );
 };
+
+
+const Wrapper = styled.div`
+  background: #FFFFFF;
+  width: 310px;
+  padding: 20px 21px;
+  border-radius: 8px;
+`;
+
+const Logo = styled.img`
+  height: 27px;
+`;
+
+const Message = styled.div`
+  margin-top: 2px;
+
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: -0.02em;
+  color: #51514D;
+`;
+
+const LinkToSignIn = styled.a`
+  display: block;
+  margin-top: 20px;
+  width: 100px;
+  height: 30px;
+  background: #7785FF;
+  border-radius: 5px;
+  padding-top: 6px;
+  margin-left: auto;
+  
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: -0.02em;
+  text-align: center;
+  color: #FFFFFF;
+  text-decoration: none;
+`;
+
 export default NotSignInPage;
