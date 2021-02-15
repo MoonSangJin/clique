0e005df28486037caa5c2db9920b161cb22ba8cc
maitracle <maitracle@gmail.com>
2021-02-15 19:47:04 +0900

    deploy: production api 서버의 주소 변경

--- src/Constants/requests.js
+++ src/Constants/requests.js
@@ -1,7 +1,7 @@
 const HostDictionaryByEnv = {
   local: 'http://localhost:8000',
   dev: 'http://ec2-52-78-239-231.ap-northeast-2.compute.amazonaws.com:8000',
-  production: 'http://ec2-15-165-122-206.ap-northeast-2.compute.amazonaws.com',
+  production: 'https://api.withclique.dev',
 };
 
-export const HOST = HostDictionaryByEnv['dev'];
+export const HOST = HostDictionaryByEnv['production'];
