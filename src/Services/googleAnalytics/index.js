25e2e916e0aec90a0e32e48592122ee6a3bae86d
maitracle <maitracle@gmail.com>
2021-03-06 23:04:55 +0900

    fix: ga event에 label에 bookmark, folder id 설정

--- src/Services/googleAnalytics/index.js
+++ src/Services/googleAnalytics/index.js
@@ -3,27 +3,24 @@ import ReactGA from 'react-ga';
 
 export const logOpenBookmarkAll = (folderId) => {
   ReactGA.event({
-    category: 'Bookmark',
-    action: 'Open all',
-    label: 'folder id',
-    value: folderId,
+    category: 'Folder',
+    action: 'Open  all - folder id',
+    label: folderId,
   });
 };
 
 export const logShareFolder = (folderId) => {
   ReactGA.event({
     category: 'Folder',
-    action: 'Share',
-    label: 'folder id',
-    value: folderId,
+    action: 'Share - folder id ',
+    label: folderId,
   });
 };
 
 export const logOpenBookmark = (bookmarkId) => {
   ReactGA.event({
     category: 'Bookmark',
-    action: 'Open a bookmark',
-    label: 'bookmark id',
-    value: bookmarkId,
+    action: 'Open - bookmark id',
+    label: bookmarkId,
   });
 };
