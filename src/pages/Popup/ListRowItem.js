17469944a432b081a0ae4fa163eb2302bfb9ade5
maitracle <maitracle@gmail.com>
2021-03-17 17:42:11 +0900

    feat: icon menu에 input 추가, 스타일링

--- src/pages/Popup/ListRowItem.js
+++ src/pages/Popup/ListRowItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import check from '../../assets/img/check.png';
 import notChecked from '../../assets/img/notChecked.png';
+import icon128 from '../../assets/img/icon-128.png';
 
 
 export default function ListRowItem({
@@ -12,10 +14,10 @@ export default function ListRowItem({
   handleClick,
 }) {
   return (
-    <ListRow key={index} onClick={handleClick}>
-      <CheckBox src={isChecked ? check : notChecked} />
-      <Image src={favIconUrl} />
-      <Title>{title}</Title>
+    <ListRow key={index}>
+      <CheckBox src={isChecked ? check : notChecked} onClick={handleClick} />
+      <Image src={favIconUrl} onError={(e) => e.target.src = icon128} onClick={handleClick} />
+      <Title value={title} onChange={() => console.log('chaged')} />
     </ListRow>
   );
 }
@@ -23,8 +25,8 @@ export default function ListRowItem({
 const ListRow = styled.div`
   display: flex;
   align-items: center;
-  height: 18px;
-  margin-bottom: 22px;
+  height: 32px;
+  margin-bottom: 8px;
 `;
 
 const CheckBox = styled.img`
@@ -37,14 +39,26 @@ const Image = styled.img`
   margin-left: 14px;
 `;
 
-const Title = styled.div`
+
+const Title = styled.input`
   display: block;
-  max-width: 175px;
-  margin-right: 35px;
+  width: 200px;
+  padding: 5px 0 7px;
+  margin-right: 11px;
   margin-left: 11px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+  height: 28px;
+  
+  
+  border: 1px solid transparent;
+  border-bottom-color: #ccc;
+  transition: 0.4s;
+  
+  :focus {
+    padding: 5px 10px 7px;
+    outline: none;
+    transition: 0.4s;
+    border: 1px solid #7785FF;
+  }
 
   font-size: 12px;
   line-height: 18px;
